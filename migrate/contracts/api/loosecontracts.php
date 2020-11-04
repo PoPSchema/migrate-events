8709700f6411c06d6486b7515a97af9155d9b066
Leonardo Losoviz <leo@getpop.org>
2020-11-04 16:17:32 +0800

    Fixed phpstan level 8

--- migrate/contracts/api/loosecontracts.php
+++ migrate/contracts/api/loosecontracts.php
@@ -3,7 +3,10 @@ use PoP\LooseContracts\AbstractLooseContractSet;
 
 class PoP_Events_LooseContracts extends AbstractLooseContractSet
 {
-	public function getRequiredNames() {
+	/**
+     * @return string[]
+     */
+    public function getRequiredNames(): array {
 		return [
 			// Options
 			'popcomponent:events:dbcolumn:orderby:events:startdate',
